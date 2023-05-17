8f2af9888f69f3cb839cbf5d2066b0d18a2edcaa
bzwwww <bzwscau@163.com>
2023-05-17 20:13:48 +0800

    first commit

---
+++ BA_3D_2D/build/CMakeFiles/BA.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/linzs/hw_day19_BA_3D_2D/BA_3D_2D/BA-3Dto2D.cpp" "CMakeFiles/BA.dir/BA-3Dto2D.cpp.o" "gcc" "CMakeFiles/BA.dir/BA-3Dto2D.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ BA_3D_2D/build/CMakeFiles/BA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "BA"
+  "BA.pdb"
+  "CMakeFiles/BA.dir/BA-3Dto2D.cpp.o"
+  "CMakeFiles/BA.dir/BA-3Dto2D.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
